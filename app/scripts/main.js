5c7468ca3cbe589a66b38f3a59603a49b8c44abd
Sebastian Krüger <sk@cron.eu>
2016-08-24 09:12:03 +0200

    Improved channel grid insertion

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,11 +13,19 @@ $(document).ready(function () {
 
     var template = $('#mixer-lane').html();
 
+    var row = null;
+
     $.each( data, function( i, item ) {
+
+      if (i % 4 === 0) {
+        row = $('<div class="row"/>');
+        $('#mixer-lanes').append(row);
+      }
+
       item.id = 'device' + i;
       item.isFirst = i === 0;
       item.label = item.name.split('@').pop();
-      $('#mixer-lanes').append(_.template(template)(item));
+      row.append(_.template(template)(item)); 
     });
 
     $('input[type="checkbox"]').customInput();
