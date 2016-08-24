7c837100c329097486dd08fba4bf63d39f6cccb2
Sebastian Krüger <sk@cron.eu>
2016-08-24 10:59:22 +0200

    Added setting of volume

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,15 +25,56 @@ $(document).ready(function () {
       item.id = 'device' + i;
       item.isFirst = i === 0;
       item.label = item.name.split('@').pop();
-      row.append(_.template(template)(item)); 
+
+      var lane = $(_.template(template)(item));
+      lane.appendTo(row);
+
+      lane.find(':checkbox')
+      .customInput()
+      .change(function() {
+        var $el = $(this);
+        var connect = $el.prop('checked');
+
+        $.ajax({
+          url: '/api/devices/' + item.name + '/connect',
+          type: 'PUT',
+          data: JSON.stringify({connect: connect}),
+          dataType: 'json',
+          contentType: 'application/json; charset=utf-8'
+        })
+        .done(function (data) {
+          $el.prop('checked', data.connected)
+          .trigger('updateState');
+        });
+      })
+      .prop('checked', item.connected)
+      .trigger('updateState');
+
+      var lastVolume = item.volume;
+      lane.find('.scrollbar').jScrollPane({
+        verticalDragMaxHeight: 36,
+        verticalDragMinHeight: 36
+      })
+      .on('jsp-scroll-y', function(event, scrollPositionY, isAtTop, isAtBottom){
+        var $el = $(this);
+        var newVolume = Math.round((1 - $el.data('jsp').getPercentScrolledY()) * 100);
+        if (newVolume === lastVolume) {
+          return;
+        }
+
+        $.ajax({
+          url: '/api/devices/' + item.name + '/volume',
+          type: 'PUT',
+          data: JSON.stringify({volume: newVolume}),
+          dataType: 'json',
+          contentType: 'application/json; charset=utf-8'
+        })
+        .done(function (data) {
+          lastVolume = data.volume;
+          $el.data('jsp').scrollToPercentY((100 - lastVolume) / 100);
+        });
+
+      }).data('jsp').scrollToPercentY((100 - lastVolume) / 100);
     });
-
-    $('input[type="checkbox"]').customInput();
-
-    $('.scrollbar.style2').jScrollPane({
-      verticalDragMaxHeight: 36,
-      verticalDragMinHeight: 36
-    });
-
   });
 });
